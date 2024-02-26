2beab47a7196138e56cbfbe2fee0c765808a80e4
Guillaume <cvr.contact.pro@gmail.com>
2024-02-26 17:02:18 +0100

    Add props to project

--- Correct/time-tracking-dashboard/src/components/Content.jsx
+++ Correct/time-tracking-dashboard/src/components/Content.jsx
@@ -1,14 +1,14 @@
-import { Item1, Item2, Item3, Item4, Item5, Item6 } from "./Items.jsx";
+import Item from "./Item.jsx";
 
 export default function Content() {
     return (
         <div className="tracks">
-            <Item1 />
-            <Item2 />
-            <Item3 />
-            <Item4 />
-            <Item5 />
-            <Item6 />
+            <Item title="Titre 1" workTime="30hrs" workPreviousTime="12hrs" />
+            <Item title="Titre 2" workTime="30hrs" workPreviousTime="10hrs" />
+            <Item title="Titre 3" workTime="36hrs" workPreviousTime="12hrs" />
+            <Item title="Titre 4" workTime="98hrs" workPreviousTime="11hrs" />
+            <Item title="Titre 5" workTime="12hrs" workPreviousTime="08hrs" />
+            <Item title="Titre 6" workTime="08hrs" workPreviousTime="29hrs" />
         </div>
     );
 }
--- Correct/time-tracking-dashboard/src/components/Profile.jsx
+++ Correct/time-tracking-dashboard/src/components/Profile.jsx
@@ -1,12 +1,10 @@
-import avatar from "/src/images/image-jeremy.png";
-
-export default function Profile() {
+export default function Profile({ firstName, lastName, avatar  }) {
     return (
         <div className="profile">
             <img src={ avatar } alt="" className="" />
                 <div>
                     <p className="stats-text">Report for</p>
-                    <p className="name">Jeremy Jhonson</p>
+                    <p className="name">{firstName} {lastName}</p>
                 </div>
         </div>
     );
