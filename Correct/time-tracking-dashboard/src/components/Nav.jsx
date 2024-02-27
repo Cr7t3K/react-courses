189b9100e765b808ff59511791721e87b7dff042
Guillaume <cvr.contact.pro@gmail.com>
2024-02-27 16:58:12 +0100

    lifting State Up

--- Correct/time-tracking-dashboard/src/components/Nav.jsx
+++ Correct/time-tracking-dashboard/src/components/Nav.jsx
@@ -1,9 +1,19 @@
-export default function Nav() {
+export default function Nav({ setCurrentTimeFrame, currentTimeFrame }) {
+
     return (
         <div className="time-selection border-bot-radius height">
-            <button id="dailyLink">Daily</button>
-            <button id="weeklyLink" className="active">Weekly</button>
-            <button id="monthlyLink">Monthly</button>
+            <button id="dailyLink" className={ currentTimeFrame === "daily" && "active"}
+                    onClick={() => (setCurrentTimeFrame("daily"))}>
+                Daily
+            </button>
+            <button id="weeklyLink" className={ currentTimeFrame === "weekly" && "active"}
+                    onClick={() => (setCurrentTimeFrame("weekly"))}>
+                Weekly
+            </button>
+            <button id="monthlyLink" className={ currentTimeFrame === "monthly" && "active"}
+                    onClick={() => (setCurrentTimeFrame("monthly"))}>
+                Monthly
+            </button>
         </div>
     );
 }
--- Correct/time-tracking-dashboard/src/components/SideBar.jsx
+++ Correct/time-tracking-dashboard/src/components/SideBar.jsx
@@ -1,11 +1,11 @@
 import Profile from "./Profile.jsx";
 import Nav from "./Nav.jsx";
 
-export default function SideBar() {
+export default function SideBar({ setCurrentTimeFrame, currentTimeFrame }) {
     return (
         <div className="sidebar">
             <Profile firstName="John" lastName="Doe" avatar="src/images/image-jeremy.png" />
-            <Nav />
+            <Nav setCurrentTimeFrame={setCurrentTimeFrame} currentTimeFrame={currentTimeFrame} />
         </div>
     );
 }
