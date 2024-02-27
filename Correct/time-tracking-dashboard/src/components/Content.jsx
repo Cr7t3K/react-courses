d13e1d7a62036044b44e8bfec2e221ee76494ff4
Guillaume <cvr.contact.pro@gmail.com>
2024-02-27 10:29:40 +0100

    Use map to iterate through data for create Component

--- Correct/time-tracking-dashboard/src/components/Content.jsx
+++ Correct/time-tracking-dashboard/src/components/Content.jsx
@@ -1,14 +1,12 @@
 import Item from "./Item.jsx";
+import datas from "../data.json"
 
 export default function Content() {
     return (
         <div className="tracks">
-            <Item title="Titre 1" workTime="30hrs" workPreviousTime="12hrs" />
-            <Item title="Titre 2" workTime="30hrs" workPreviousTime="10hrs" />
-            <Item title="Titre 3" workTime="36hrs" workPreviousTime="12hrs" />
-            <Item title="Titre 4" workTime="98hrs" workPreviousTime="11hrs" />
-            <Item title="Titre 5" workTime="12hrs" workPreviousTime="08hrs" />
-            <Item title="Titre 6" workTime="08hrs" workPreviousTime="29hrs" />
+            {datas.map((data) => (
+                <Item key={data.id} title={data.title} workTime={data.timeframes.monthly.current} workPreviousTime={data.timeframes.monthly.previous} />
+            ))}
         </div>
     );
 }
