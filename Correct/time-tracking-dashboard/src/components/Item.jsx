61d36b28c3b0492080625768344a6116421816b0
Guillaume <cvr.contact.pro@gmail.com>
2024-02-27 14:05:12 +0100

    use State for favorite Item

--- Correct/time-tracking-dashboard/src/components/Item.jsx
+++ Correct/time-tracking-dashboard/src/components/Item.jsx
@@ -1,11 +1,16 @@
-import icon from "/src/images/icon-ellipsis.svg"
+import FavoriteIcon from "./FavoriteIcon.jsx";
+import { useState } from "react";
 
 export default function Item ({ title, workTime, workPreviousTime }) {
+    const [isFavorite, setIsFavorite] = useState(false)
+
     return (
         <div className="track-item">
             <div className="nav">
                 <span id="workTitle">{ title }</span>
-                <span><img src={ icon } alt="" /></span>
+                <button onClick={() => (setIsFavorite(!isFavorite))}>
+                    <FavoriteIcon isFavorite={isFavorite} />
+                </button>
             </div>
             <p className="time" id="workTime">{ workTime }</p>
             <p className="previous" id="workPrevious">Last week - { workPreviousTime }</p>
